efdbfe5cd575d1c2038c491ffbede37e52fd1f50
TuanHuy185 <huy.lamtuan1805@hcmut.edu.vn>
2025-02-24 14:43:41 +0700

    fix(fe): use navigate instead of window in login form

--- frontend-react/src/components/root/Login/LoginForm.jsx
+++ frontend-react/src/components/root/Login/LoginForm.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import InputField from "../fragments/InputField/InputField";
-import { toast } from "react-toastify";
-import { jwtDecode } from "jwt-decode";
 
 function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
@@ -11,30 +9,8 @@ function LoginForm() {
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
-    window.location.href = "/account";
-
-    // event.preventDefault();
-    // const reqJson = { username: email, password: password };
-
-    // fetch(`${import.meta.env.VITE_REACT_APP_BE_API_URL}/api/authenticate`, {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify(reqJson),
-    //   credentials: "include",
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     if (data.status === "OK") {
-    //       const decodedToken = jwtDecode(data.data);
-    //       const role = decodedToken.role;
-    //       localStorage.setItem("token", data.data);
-    //       localStorage.setItem("userRole", role);
-    //       window.location.href = "/account";
-    //     } else {
-    //       toast.error("Đăng nhập thất bại. Vui lòng kiểm tra lại thông tin.");
-    //     }
-    //   })
-    //   .catch(() => toast.error("Đã xảy ra lỗi khi đăng nhập."));
+    event.preventDefault(); // Ngăn chặn reload trang
+    navigate("/account"); // Chuyển hướng đến trang Account
   };
 
   return (
@@ -63,7 +39,6 @@ function LoginForm() {
             required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            icon="https://cdn.builder.io/api/v1/image/assets/TEMP/0acec3f0c21c585b693aab238ddf1a6054cfa9ee7646ac7df643f1272897cf03"
           />
 
           <InputField
@@ -74,7 +49,6 @@ function LoginForm() {
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            icon="https://cdn.builder.io/api/v1/image/assets/TEMP/26d9c5399a667e190537f967c908e5b53fea2716a5b94db02389112e242bc353"
             showPasswordToggle
             showPassword={showPassword}
             onTogglePassword={() => setShowPassword(!showPassword)}
