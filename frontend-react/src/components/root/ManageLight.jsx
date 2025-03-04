c8fc9c8a276f5475c51c73877637cb6f8b06f872
TuanHuy185 <huy.lamtuan1805@hcmut.edu.vn>
2025-03-04 14:29:14 +0700

    build(fe): create page ManageLight

---
+++ frontend-react/src/components/root/ManageLight.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "./fragments/layout/Layout";
+import ManageLight from './ManageLight/ManageLight';
+
+function Manage() {
+    return (
+        <Layout>
+            <ManageLight/>
+        </Layout>
+    );
+}
+
+export default Manage;
