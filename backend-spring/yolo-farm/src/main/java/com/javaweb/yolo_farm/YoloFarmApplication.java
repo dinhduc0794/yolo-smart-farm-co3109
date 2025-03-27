515c8b0040fb82da8851b4d75ca7c76da64e6f3f
dinhduc0794 <ngdduc79@gmail.com>
2025-03-27 11:39:13 +0700

    build: mongodb config

---
+++ backend-spring/yolo-farm/src/main/java/com/javaweb/yolo_farm/YoloFarmApplication.java
@@ -0,0 +1,13 @@
+package com.javaweb.yolo_farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoloFarmApplication.class, args);
+	}
+
+}
---
+++ backend-spring/yolo-farm/src/test/java/com/javaweb/yolo_farm/YoloFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb.yolo_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoloFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
