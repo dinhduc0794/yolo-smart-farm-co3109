85b8f95b879166ba46bce67d2bb5365e271b38a4
doandinhhao <doandinhhao2004@gmail.com>
2025-05-14 21:13:04 +0700

    fix: add endpoint for cors

--- backend-spring/yolo-farm/src/main/java/com/javaweb/yolo_farm/config/SecurityConfig.java
+++ backend-spring/yolo-farm/src/main/java/com/javaweb/yolo_farm/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:8085" ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
